ccbb7680de9b4ab2c60ce4e94c98fdb9be2720f1
Admin <Admin@DESKTOP-2NSIB7R>
2023-03-29 17:07:16 +0530

    hi it is now second commit

--- src/ProjectDemoVersion/Tc2.java
+++ src/ProjectDemoVersion/Tc2.java
@@ -1,6 +1,6 @@
 package ProjectDemoVersion;
 
-public class Tc1 {
+public class Tc2 {
 
 	public static void main(String[] args) {
 		System.out.println("hello test case 01");
@@ -10,7 +10,6 @@ public class Tc1 {
 		System.out.println("hello test case 02");
 		System.out.println("hello test case 02");
 		System.out.println("hello test case 03");
-		System.out.println("hello test case 04");
 
 	}
 
