fe9ba37c9247e65ca763e3c54d4e489ebda25167
Admin <Admin@DESKTOP-2NSIB7R>
2023-03-29 16:38:35 +0530

    hi first commit is done.......................

---
+++ src/ProjectDemoVersion/Tc1.java
@@ -0,0 +1,13 @@
+package ProjectDemoVersion;
+
+public class Tc1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello test case 01");
+		System.out.println("hello test case 01");
+		System.out.println("hello test case 01");
+		System.out.println("hello test case 01");
+
+	}
+
+}
